009331a0f83defeb64e5bb62c556bc2480bdcc82
R <r@MacBook-Pro-R.local>
2025-02-11 19:54:34 +0300

    Added model News.

--- src/backend/news/models.py
+++ src/backend/news/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    short_description = models.CharField(max_length=255, verbose_name="Краткое описание")
+    content = models.TextField(verbose_name="Содержание")
+    image = models.ImageField(upload_to='news/', null=True, blank=True, verbose_name="Изображение")
+    published_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата публикации")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Новость"
+        verbose_name_plural = "Новости"
