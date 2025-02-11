7761fbd351153f25fb2021cb1de38995728d3f14
R <r@MacBook-Pro-R.local>
2025-02-11 19:15:32 +0300

    Added models Restaurant.

--- src/backend/restaurants/models.py
+++ src/backend/restaurants/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Название")
+    address = models.CharField(max_length=255, verbose_name="Адрес")
+    phone = models.CharField(max_length=20, verbose_name="Телефон")
+    description = models.TextField(verbose_name="Описание")
+    cuisine_type = models.CharField(max_length=50, verbose_name="Тип кухни")  # Например, "итальянская", "японская"
+    image = models.ImageField(upload_to='restaurants/', null=True, blank=True, verbose_name="Изображение")
+    rating = models.DecimalField(max_digits=3, decimal_places=2, default=0.0, verbose_name="Рейтинг")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Ресторан"
+        verbose_name_plural = "Рестораны"
